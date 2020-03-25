1acde9bb6a108267276907b18bb4e2af9586e82c
James Craig <craig.jamesa@gmail.com>
2020-03-25 14:34:24 -0400

    Package updates and adding test app.

---
+++ TestApp/Modules/LoggingModule.cs
@@ -0,0 +1,32 @@
+﻿using Canister.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace TestApp.Modules
+{
+    public class LoggingModule : IModule
+    {
+        /// <summary>
+        /// Order to run this in
+        /// </summary>
+        public int Order => 1;
+
+        /// <summary>
+        /// Loads the module using the bootstrapper
+        /// </summary>
+        /// <param name="bootstrapper">The bootstrapper.</param>
+        public void Load(IBootstrapper bootstrapper)
+        {
+            if (bootstrapper is null)
+                return;
+            Log.Logger = new LoggerConfiguration()
+                                            .WriteTo
+                                            .File("./Log.txt")
+                                            .MinimumLevel
+                                            .Debug()
+                                            .CreateLogger();
+            bootstrapper.Register<ILogger>(Log.Logger,
+                                        ServiceLifetime.Singleton);
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,146 @@
+﻿using BigBook;
+using Microsoft.Extensions.Configuration;
+using SQLHelperDB;
+using SQLHelperDB.Registration;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Canister.Builder.CreateContainer(null).AddAssembly(typeof(Program).Assembly).RegisterSQLHelper().Build();
+
+            var Results = new SQLHelper(Canister.Builder.Bootstrapper.Resolve<IConfiguration>(), SqlClientFactory.Instance)
+                .AddQuery(CommandType.Text, @";WITH MyDuplicate AS (SELECT
+Sch.[name] AS SchemaName,
+Obj.[name] AS TableName,
+Idx.[name] AS IndexName,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 1) AS Col1,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 2) AS Col2,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 3) AS Col3,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 4) AS Col4,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 5) AS Col5,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 6) AS Col6,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 7) AS Col7,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 8) AS Col8,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 9) AS Col9,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 10) AS Col10,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 11) AS Col11,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 12) AS Col12,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 13) AS Col13,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 14) AS Col14,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 15) AS Col15,
+INDEX_COL(Sch.[name] + '.' + Obj.[name], Idx.index_id, 16) AS Col16
+FROM sys.indexes Idx
+INNER JOIN sys.objects Obj ON Idx.[object_id] = Obj.[object_id] INNER JOIN sys.schemas Sch ON Sch.[schema_id] = Obj.[schema_id] WHERE index_id > 0)
+SELECT    MD1.SchemaName, MD1.TableName, MD1.IndexName,
+MD2.IndexName AS OverLappingIndex,
+MD1.Col1, MD1.Col2, MD1.Col3, MD1.Col4,
+MD1.Col5, MD1.Col6, MD1.Col7, MD1.Col8,
+MD1.Col9, MD1.Col10, MD1.Col11, MD1.Col12,
+MD1.Col13, MD1.Col14, MD1.Col15, MD1.Col16
+FROM MyDuplicate MD1
+INNER JOIN MyDuplicate MD2 ON MD1.tablename = MD2.tablename
+AND MD1.indexname <> MD2.indexname
+AND MD1.Col1 = MD2.Col1
+AND (MD1.Col2 IS NULL OR MD2.Col2 IS NULL OR MD1.Col2 = MD2.Col2)
+AND (MD1.Col3 IS NULL OR MD2.Col3 IS NULL OR MD1.Col3 = MD2.Col3)
+AND (MD1.Col4 IS NULL OR MD2.Col4 IS NULL OR MD1.Col4 = MD2.Col4)
+AND (MD1.Col5 IS NULL OR MD2.Col5 IS NULL OR MD1.Col5 = MD2.Col5)
+AND (MD1.Col6 IS NULL OR MD2.Col6 IS NULL OR MD1.Col6 = MD2.Col6)
+AND (MD1.Col7 IS NULL OR MD2.Col7 IS NULL OR MD1.Col7 = MD2.Col7)
+AND (MD1.Col8 IS NULL OR MD2.Col8 IS NULL OR MD1.Col8 = MD2.Col8)
+AND (MD1.Col9 IS NULL OR MD2.Col9 IS NULL OR MD1.Col9 = MD2.Col9)
+AND (MD1.Col10 IS NULL OR MD2.Col10 IS NULL OR MD1.Col10 = MD2.Col10)
+AND (MD1.Col11 IS NULL OR MD2.Col11 IS NULL OR MD1.Col11 = MD2.Col11)
+AND (MD1.Col12 IS NULL OR MD2.Col12 IS NULL OR MD1.Col12 = MD2.Col12)
+AND (MD1.Col13 IS NULL OR MD2.Col13 IS NULL OR MD1.Col13 = MD2.Col13)
+AND (MD1.Col14 IS NULL OR MD2.Col14 IS NULL OR MD1.Col14 = MD2.Col14)
+AND (MD1.Col15 IS NULL OR MD2.Col15 IS NULL OR MD1.Col15 = MD2.Col15)
+AND (MD1.Col16 IS NULL OR MD2.Col16 IS NULL OR MD1.Col16 = MD2.Col16)
+ORDER BY MD1.SchemaName,MD1.TableName,MD1.IndexName")
+                .AddQuery(CommandType.Text, @"SELECT TOP 25
+dm_mid.database_id AS DatabaseID,
+dm_migs.avg_user_impact*(dm_migs.user_seeks+dm_migs.user_scans) Avg_Estimated_Impact,
+dm_migs.last_user_seek AS Last_User_Seek,
+OBJECT_NAME(dm_mid.OBJECT_ID,dm_mid.database_id) AS [TableName],
+'CREATE INDEX [IX_' + OBJECT_NAME(dm_mid.OBJECT_ID,dm_mid.database_id) + '_'
++ REPLACE(REPLACE(REPLACE(ISNULL(dm_mid.equality_columns,''),', ','_'),'[',''),']','')
++ CASE
+WHEN dm_mid.equality_columns IS NOT NULL
+AND dm_mid.inequality_columns IS NOT NULL THEN '_'
+ELSE ''
+END
++ REPLACE(REPLACE(REPLACE(ISNULL(dm_mid.inequality_columns,''),', ','_'),'[',''),']','')
++ ']'
++ ' ON ' + dm_mid.statement
++ ' (' + ISNULL (dm_mid.equality_columns,'')
++ CASE WHEN dm_mid.equality_columns IS NOT NULL AND dm_mid.inequality_columns
+IS NOT NULL THEN ',' ELSE
+'' END
++ ISNULL (dm_mid.inequality_columns, '')
++ ')'
++ ISNULL (' INCLUDE (' + dm_mid.included_columns + ')', '') AS Create_Statement
+FROM sys.dm_db_missing_index_groups dm_mig
+INNER JOIN sys.dm_db_missing_index_group_stats dm_migs
+ON dm_migs.group_handle = dm_mig.index_group_handle
+INNER JOIN sys.dm_db_missing_index_details dm_mid
+ON dm_mig.index_handle = dm_mid.index_handle
+WHERE dm_mid.database_ID = DB_ID()
+ORDER BY Avg_Estimated_Impact DESC")
+                .AddQuery(CommandType.Text, @"SELECT TOP 25 SUBSTRING(qt.TEXT, (qs.statement_start_offset/2)+1,
+((CASE qs.statement_end_offset
+WHEN -1 THEN DATALENGTH(qt.TEXT)
+ELSE qs.statement_end_offset
+END - qs.statement_start_offset)/2)+1) as [query_text],
+qs.execution_count,
+qs.total_logical_reads, qs.last_logical_reads,
+qs.total_logical_writes, qs.last_logical_writes,
+qs.total_worker_time,
+qs.last_worker_time,
+qs.total_elapsed_time/1000000 total_elapsed_time_in_S,
+qs.last_elapsed_time/1000000 last_elapsed_time_in_S,
+qs.last_execution_time
+FROM sys.dm_exec_query_stats qs CROSS APPLY sys.dm_exec_sql_text(qs.plan_handle) st
+CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) qt
+CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
+where DB_NAME(st.dbid)=DB_NAME()
+ORDER BY qs.total_worker_time DESC")
+                .AddQuery(CommandType.Text, @"SELECT TOP 25
+o.name AS ObjectName
+, i.name AS IndexName
+, i.index_id AS IndexID
+, dm_ius.user_seeks AS UserSeek
+, dm_ius.user_scans AS UserScans
+, dm_ius.user_lookups AS UserLookups
+, dm_ius.user_updates AS UserUpdates
+, p.TableRows
+, 'DROP INDEX ' + QUOTENAME(i.name)
++ ' ON ' + QUOTENAME(s.name) + '.'
++ QUOTENAME(OBJECT_NAME(dm_ius.OBJECT_ID)) AS 'drop statement'
+FROM sys.dm_db_index_usage_stats dm_ius
+INNER JOIN sys.indexes i ON i.index_id = dm_ius.index_id
+AND dm_ius.OBJECT_ID = i.OBJECT_ID
+INNER JOIN sys.objects o ON dm_ius.OBJECT_ID = o.OBJECT_ID
+INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+INNER JOIN(SELECT SUM(p.rows) TableRows, p.index_id, p.OBJECT_ID
+FROM sys.partitions p GROUP BY p.index_id, p.OBJECT_ID) p
+ON p.index_id = dm_ius.index_id AND dm_ius.OBJECT_ID = p.OBJECT_ID
+WHERE OBJECTPROPERTY(dm_ius.OBJECT_ID, 'IsUserTable') = 1
+AND dm_ius.database_id = DB_ID()
+AND i.type_desc = 'nonclustered'
+AND i.is_primary_key = 0
+AND i.is_unique_constraint = 0
+AND(dm_ius.user_scans + dm_ius.user_lookups) > dm_ius.user_seeks
+ORDER BY(dm_ius.user_scans + dm_ius.user_lookups) DESC")
+                .Execute();
+
+            Console.WriteLine("These are the most expensive queries by total CPU time found.");
+            Console.WriteLine(Results[3].ToString(x => x.ToString()));
+        }
+    }
+}
